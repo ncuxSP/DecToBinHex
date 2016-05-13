3fdf771751d3d7f573e244518391b8194c2b2281
ncuxSP <ncuxsp@gmail.com>
2016-05-14 00:17:16 +0700

    Initial commit

---
+++ DecToBinHexTool/MainForm.Designer.cs
@@ -0,0 +1,89 @@
+﻿namespace DecToBinHexTool
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.AddButton = new System.Windows.Forms.Button();
+            this.InputTextBox = new System.Windows.Forms.TextBox();
+            this.ResultList = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // AddButton
+            // 
+            this.AddButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.AddButton.Location = new System.Drawing.Point(190, 40);
+            this.AddButton.Name = "AddButton";
+            this.AddButton.Size = new System.Drawing.Size(80, 20);
+            this.AddButton.TabIndex = 0;
+            this.AddButton.Text = "Calculate";
+            this.AddButton.UseVisualStyleBackColor = true;
+            // 
+            // InputTextBox
+            // 
+            this.InputTextBox.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.InputTextBox.HideSelection = false;
+            this.InputTextBox.Location = new System.Drawing.Point(30, 40);
+            this.InputTextBox.Name = "InputTextBox";
+            this.InputTextBox.Size = new System.Drawing.Size(150, 20);
+            this.InputTextBox.TabIndex = 1;
+            // 
+            // ResultList
+            // 
+            this.ResultList.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.ResultList.FormattingEnabled = true;
+            this.ResultList.Location = new System.Drawing.Point(30, 80);
+            this.ResultList.Name = "ResultList";
+            this.ResultList.Size = new System.Drawing.Size(240, 173);
+            this.ResultList.TabIndex = 2;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(304, 281);
+            this.Controls.Add(this.ResultList);
+            this.Controls.Add(this.InputTextBox);
+            this.Controls.Add(this.AddButton);
+            this.MinimumSize = new System.Drawing.Size(320, 320);
+            this.Name = "MainForm";
+            this.Text = "DecToBinHex";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button AddButton;
+        private System.Windows.Forms.TextBox InputTextBox;
+        private System.Windows.Forms.ListBox ResultList;
+    }
+}
